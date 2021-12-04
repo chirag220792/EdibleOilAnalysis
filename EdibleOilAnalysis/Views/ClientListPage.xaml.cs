b099c9f2c26437afe6bcac55b02dec0df3c410ac
apple <chirag.pokiya@rbkschool.org>
2021-12-04 20:04:25 +0530

    ClientPageAdded

---
+++ EdibleOilAnalysis/Views/ClientListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EdibleOilAnalysis.Views
+{
+    public partial class ClientListPage : ContentPage
+    {
+        public ClientListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
